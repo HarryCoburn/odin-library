2526eb1b8a0f2ab9dd70cf463f38864640d0c377
mattibun <mattibun@gmail.com>
2019-09-11 14:08:17 -0400

    created Render function

--- library.js
+++ library.js
@@ -1,4 +1,7 @@
-let myLibrary = [];
+let myLibrary = [
+    {title: 'The Hobbit'}, 
+    {title: 'The Fellowship of the Ring'}
+];
 
 function Book(title, author, pageCount, readYet) {
     this.title = title    
@@ -17,3 +20,24 @@ function addBookToLibrary() {
     let readYet = "false"
     myLibrary.push(new Book(title, author, pageCount, readYet));
 }
+
+
+function render(myLibrary) {
+    let body = document.querySelector('body');
+    myLibrary.forEach(book => {
+        let newBook = document.createElement('div');
+        let bookDetails = document.createElement('div');
+        let title = document.createElement('h1');        
+
+        title.textContent = book.title;
+        bookDetails.innerHTML = `
+        Author: ${book.author}<br>
+        Page Count: ${book.pageCount}<br>
+        Read yet?: ${book.readYet}`        
+        newBook.appendChild(title) 
+        newBook.appendChild(bookDetails)       
+        body.appendChild(newBook);
+    });
+}
+
+render(myLibrary)
