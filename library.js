d2c95a116e4430199ec4ad0a9603c9d516d4fbfb
mattibun <mattibun@gmail.com>
2019-09-11 14:25:48 -0400

    improved CSS. Starting button logic

--- library.js
+++ library.js
@@ -1,15 +1,15 @@
 let myLibrary = [
-    {title: 'The Hobbit'}, 
-    {title: 'The Fellowship of the Ring'}
+    { title: 'The Hobbit', author: "J.R.R. Tolkien", pageCount: 264, readYet: true },
+    { title: 'The Fellowship of the Ring' }
 ];
 
 function Book(title, author, pageCount, readYet) {
-    this.title = title    
+    this.title = title
     this.author = author
     this.pageCount = pageCount
     this.readYet = readYet
-    this.info = function() {
-        return `${title} by ${author}, ${pageCount} pages, ${readYet?"already read":"not read yet"}`
+    this.info = function () {
+        return `${title} by ${author}, ${pageCount} pages, ${readYet ? "already read" : "not read yet"}`
     }
 }
 
@@ -26,16 +26,18 @@ function render(myLibrary) {
     let body = document.querySelector('body');
     myLibrary.forEach(book => {
         let newBook = document.createElement('div');
+        newBook.classList.add("bookEntry");
         let bookDetails = document.createElement('div');
-        let title = document.createElement('h1');        
+        bookDetails.classList.add("bookDetails");
+        let title = document.createElement('h2');
 
         title.textContent = book.title;
         bookDetails.innerHTML = `
         Author: ${book.author}<br>
         Page Count: ${book.pageCount}<br>
-        Read yet?: ${book.readYet}`        
-        newBook.appendChild(title) 
-        newBook.appendChild(bookDetails)       
+        Read yet?: ${book.readYet}`
+        newBook.appendChild(title)
+        newBook.appendChild(bookDetails)
         body.appendChild(newBook);
     });
 }
