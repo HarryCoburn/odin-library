6f761093110110301c03aee5e7fd360c67808baa
mattibun <mattibun@gmail.com>
2019-09-12 00:33:10 -0400

    Adding new book form logic

--- library.js
+++ library.js
@@ -14,16 +14,19 @@ function Book(title, author, pageCount, readYet) {
 }
 
 function addBookToLibrary() {
+    console.log("Got here");
     let title = "foo"
     let author = "bar"
     let pageCount = "baz"
     let readYet = "false"
     myLibrary.push(new Book(title, author, pageCount, readYet));
+    render(myLibrary);
 }
 
 
 function render(myLibrary) {
-    let body = document.querySelector('body');
+    let bookList = document.querySelector('#bookList');
+    destroyOldList(bookList);
     myLibrary.forEach(book => {
         let newBook = document.createElement('div');
         newBook.classList.add("bookEntry");
@@ -38,8 +41,45 @@ function render(myLibrary) {
         Read yet?: ${book.readYet}`
         newBook.appendChild(title)
         newBook.appendChild(bookDetails)
-        body.appendChild(newBook);
+        bookList.appendChild(newBook);
     });
+}
+
+function destroyOldList(bookList) {
+    let child = bookList.lastElementChild;
+    while (child) {
+        bookList.removeChild(child);
+        child = bookList.lastElementChild;
+    }
+}
+
+bookFormButton = document.querySelector('button[id="newBook"]');
+bookFormButton.addEventListener('click', openBookForm);
+
+function openBookForm() {
+    bookFormButton.style.visibility = "hidden";
+    let formDiv = document.querySelector('div[id="addBookForm"');    
+    let newBookForm = document.createElement('form');    
+    // Populate form with fields
+    let fieldNames = ["Title", "Author", "Page Count", "Read Yet?"]
+    fieldNames.forEach(field => {
+        let newField = document.createElement('input');
+        newField.id = field;
+        let newLabel = document.createElement('label');
+        newLabel.for= field;
+        newLabel.textContent = field + ": ";
+        newField.type = "text";
+        newField.name = field;
+        newField.id = field;
+        newBookForm.appendChild(newLabel);
+        newBookForm.appendChild(newField);
+    })
+    let submitButton = document.createElement('button');    
+    submitButton.textContent = "Add Book"
+    submitButton.addEventListener("click", addBookToLibrary);
+        
+    formDiv.appendChild(newBookForm);
+    formDiv.appendChild(submitButton);
 }
 
 render(myLibrary)
